e7cbb3871989e3da56c8ef0c1b66c1f8df84e9a2
Christian Skorobogatow <christiansko@icloud.com>
2021-06-06 17:10:20 +0200

    Added TabView again due pathing error in GIT

---
+++ DubDubGrub/AppTabView.swift
@@ -0,0 +1,35 @@
+//
+//  AppTabView.swift
+//  DubDubGrub
+//
+//  Created by Christian Skorobogatow on 6/6/21.
+//
+
+import SwiftUI
+
+struct AppTabView: View {
+    var body: some View {
+        TabView {
+            LocationMapView()
+                .tabItem {
+                    Label("Map", systemImage: "map")
+                }
+            
+            LocationListView()
+                .tabItem {
+                    Label("Locations", systemImage: "building")
+                }
+            
+            ProfileVIew()
+                .tabItem {
+                    Label("Profile", systemImage: "person")
+                }
+        }
+    }
+}
+
+struct AppTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        AppTabView()
+    }
+}
