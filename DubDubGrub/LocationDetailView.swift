8a1ff67ff015f7630f4556976d4699105c886733
Christian Skorobogatow <christiansko@icloud.com>
2021-06-17 22:43:22 +0200

    Feat: Basic mockup of UI for LocationList View

---
+++ DubDubGrub/LocationDetailView.swift
@@ -0,0 +1,31 @@
+//
+//  LocationDetailView.swift
+//  DubDubGrub
+//
+//  Created by Christian Skorobogatow on 17/6/21.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        VStack{
+            Image("default-banner-asset")
+            
+            Text("Description goes here")
+            
+            HStack{
+                Text("4 Buttons here")
+            }
+            
+            
+            
+        }
+    }
+}
+
+struct LocationDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationDetailView()
+    }
+}
--- DubDubGrub/LocationListView.swift
+++ DubDubGrub/LocationListView.swift
@@ -9,12 +9,37 @@ import SwiftUI
 
 struct LocationListView: View {
     var body: some View {
-        Text("Location List Goes here!")
+        List {
+            CellView()
+        }
     }
 }
 
 struct LocationListView_Previews: PreviewProvider {
     static var previews: some View {
         LocationListView()
+    }
+}
+
+struct CellView: View {
+    var body: some View {
+        HStack{
+            Circle()
+                .frame(width: 100, height: 100)
+            
+            VStack{
+                Text("Title goes here")
+                    .font(.body)
+                HStack{
+                    Circle()
+                        .frame(width: 50, height: 50)
+                    Circle()
+                        .frame(width: 50, height: 50)
+                    Circle()
+                        .frame(width: 50, height: 50)
+                }
+                
+            }
+        }.navigationTitle("Test")
     }
 }
