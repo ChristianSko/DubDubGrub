34e366cdb7045df866494eaf8a871efc84b883cc
Christian Skorobogatow <christiansko@icloud.com>
2021-06-18 15:27:28 +0200

    Feat: Second draft of of LocationDetailView

--- DubDubGrub/LocationListView.swift
+++ DubDubGrub/LocationListView.swift
@@ -29,22 +29,37 @@ struct LocationListView_Previews: PreviewProvider {
 struct CellView: View {
     var body: some View {
         HStack{
-            Circle()
-                .frame(width: 100, height: 100)
+            Image("default-square-asset")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 80, height: 80)
+                .clipShape(Circle())
+                .padding(.vertical, 8)
             
             VStack{
-                Text("Title goes here")
-                    .font(.body)
-                HStack{
-                    Circle()
-                        .frame(width: 50, height: 50)
-                    Circle()
-                        .frame(width: 50, height: 50)
-                    Circle()
-                        .frame(width: 50, height: 50)
-                }
+                Text("Location goes here")
+                    .font(.title2)
+                    .fontWeight(.semibold)
+                    .lineLimit(1)
+                    .minimumScaleFactor(0.75)
                 
+                HStack{
+                    ForEach(0..<3){ user in
+                        AvatarView()
+                    }
+                }
             }
+            .padding(.leading)
         }.navigationTitle("Test")
+    }
+}
+
+struct AvatarView: View {
+    var body: some View {
+        Image("default-avatar")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 35, height: 35)
+            .clipShape(Circle())
     }
 }
