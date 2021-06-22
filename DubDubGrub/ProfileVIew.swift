d97c145f163c9ae6029ee5c0b2754f8b4126391d
Christian Skorobogatow <christiansko@icloud.com>
2021-06-22 23:40:58 +0200

    feat: Profile View name section setup

--- DubDubGrub/ProfileVIew.swift
+++ DubDubGrub/ProfileVIew.swift
@@ -8,13 +8,65 @@
 import SwiftUI
 
 struct ProfileVIew: View {
+    @State var firstName    = ""
+    @State var lastName     = ""
+    @State var companyName  = ""
+    @State var bio    = ""
+    
     var body: some View {
-        Text("Profile View goes here")
+        VStack{
+            ZStack{
+                Color(.secondarySystemBackground)
+                    .frame(height: 138)
+                    .cornerRadius(12)
+                    .padding(.horizontal)
+                
+                HStack(spacing: 16) {
+                    ZStack {
+                        AvatarView(size: 84)
+                            
+
+                        Image(systemName: "square.and.pencil")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 14, height: 14)
+                            .foregroundColor(.white)
+                            .offset(y: 38)
+                        
+                    }
+                    .padding(.leading)
+                    
+                    VStack(spacing: 1){
+                        TextField("First name", text: $firstName)
+                            .font(.system(size: 32, weight: .bold))
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.75)
+                        
+                        TextField("Last name", text: $lastName)
+                            .font(.system(size: 32, weight: .bold))
+                            .lineLimit(1)
+                            .minimumScaleFactor(0.75)
+                        
+                        TextField("Company name", text: $companyName)
+                            
+                    }
+                    .padding(.trailing)
+    
+                    
+                }
+                .padding()
+            }
+            
+            Spacer()
+        }
+        .navigationTitle("Profile")
     }
 }
 
 struct ProfileVIew_Previews: PreviewProvider {
     static var previews: some View {
-        ProfileVIew()
+        NavigationView{
+            ProfileVIew(firstName: "", lastName: "", companyName: "", bio: "")
+        }
     }
 }
